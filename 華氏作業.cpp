06717ecd278b932983d25b0e829fbc1cbd120abc
liujimmy0124 <56680290+liujimmy0124@users.noreply.github.com>
2019-11-17 17:12:05 +0800

    華氏作業.cpp

---
+++ 華氏作業.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "請輸入攝氏";
+
+    int C;
+    cin >> C;
+
+    int F;
+    F = ( (C * 9) / 5) + 32;
+    cout << F;
+}
+//655495 劉品毅
